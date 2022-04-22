f13576d175f26e7bf5a939320211e2079e225b1c
Aldo pereira <aldopereira.gyn@gmail.com>
2022-04-22 16:57:01 -0300

    Configuração do projeto

---
+++ src/server.js
@@ -0,0 +1,6 @@
+const app = require('../src/app')
+
+
+app.listen(3131, () => {
+    console.log("Servidor rodando com sucesso!")
+})
---
+++ test/server.test.js
@@ -0,0 +1,14 @@
+const app = require('../src/app')
+const supertest = require('supertest')
+const request = supertest(app)
+
+test("A Aplicação deve responder na porta 3131", () => {
+
+    return request.get("/").then(res => {
+        let status = res.statusCode
+        expect(status).toEqual(200)
+    }).catch(err => {
+        fail(err)
+    })
+
+})
