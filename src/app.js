ee4ad80ecb06abfd627f65e02ed2ee1ba6667e4c
Aldo pereira <aldopereira.gyn@gmail.com>
2022-04-22 17:33:12 -0300

    Configuração do banco de dados mongo

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
-
+const mongoose = require('mongoose')
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+mongoose.connect("mongodb://loclhost:27017/guiapics", { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.get("/", (req, res) => {
     res.json({})
