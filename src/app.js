698024f5d3ee7fd6d868b084a9be67aee07a3bc5
Aldo pereira <aldopereira.gyn@gmail.com>
2022-04-26 11:51:05 -0300

    Resolvendo o teste cadastro de usuário

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const user = require("./models/User")
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
@@ -11,8 +12,19 @@ mongoose.connect("mongodb://172.17.0.2:27017/guiapics", { useNewUrlParser: true,
     }).catch((err) => {
         // console.log(err)
     })
-
+const User = mongoose.model("User", user)
 app.get("/", (req, res) => {
     res.json({})
+})
+
+app.post("/user", async(req, res) => {
+    try {
+        let newUser = new User({ name: req.body.name, email: req.body.email, password: req.body.password })
+        await newUser.save()
+        res.json({ email: req.body.emai })
+    } catch (error) {
+        res.sendStatus(500)
+    }
+
 })
 module.exports = app
